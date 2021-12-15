03685cc32a6587c2b781fb779bfb581a11a28094
LifeStyle-RP <91337702+LifeStyle-RP@users.noreply.github.com>
2021-12-15 20:44:07 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,399 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Life Style</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  <link href="images/LS-RP.png" rel="icon">
+  <link href="images/LS-RP.png" rel="apple-touch-icon">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+  <link href="images/LS-RP.png" rel="icon">
+
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/styly.css">
+
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+  
+
+  <script src="https://apis.google.com/js/platform.js"></script>
+  <script type="text/javascript" src="assets/js/jshere.js"></script>
+  <script type="text/javascript" src="assets/js/free.js"></script>
+
+
+
+
+
+
+  <link href="assets-pre/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets-pre/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets-pre/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets-pre/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets-pre/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets-pre/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets-pre/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <div id="preloader"><center><img src="https://cdn.discordapp.com/attachments/889213230872928276/912458092749418526/new_LS-3D-v7.gif" alt="" id="pr" ></center></div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" name="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        
+        <h1 class="text-light"><a href="index.html"><span><b>L<span class="text-white typed" id="type-ugrpg" data-type-this="ife Style"></span></b>
+          <script src="https://ugsamp.com/assets/js/purpose.core.js"></script>
+            <script src="https://ugsamp.com/assets/libs/typed.js/lib/typed.min.js"></script>
+            <script src="https://ugsamp.com/assets/js/purpose.js"></script></span></a></h1>
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" name="navbar" class="navbar">
+        <ul>
+          <li><a class="active " href="index.html"><i class="fas fa-home" ></i>&nbsp; Home</a></li>
+          <li><a href="rules.html"><i class="fad fa-tasks"></i></i></i></i></i>&nbsp;Rules</a></li>
+          <li><a href="launcher.html"><i class="fab fa-linux"></i>&nbsp;Launcher</a></li>
+          <li class="dropdown"><a href="#"><span><i class="fab fa-stack-overflow"></i>&nbsp;Whitelist</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Whitelist App</a></li>
+              <li><a href="#">Organization App</a></li>
+              <li><a href="#">Police App</a></li>
+              <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfGNLWwmvqLD8xg3EJ1X9xaZAYI-bvTdDEaJQ9DYdLyDFyLFQ/viewform">Staff App</a></li>
+            </ul>
+          </li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+  <main>
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex justify-cntent-center align-items-center">
+    <div id="heroCarousel" class="container carousel carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+
+      <!-- Slide 1 -->
+      <div class="carousel-item active">
+        <div class="carousel-container">
+          <img src="https://cdn.discordapp.com/attachments/889213230872928276/912458572116422676/ls_new_logo.png" alt="" class="herelogo">
+         <img src="https://cdn.discordapp.com/attachments/859173547997265921/896720034372669460/2.png" alt="" class="imgslide" > 
+        </div>
+      </div>
+
+      <!-- Slide 2 -->
+      <div class="carousel-item">
+        <div class="carousel-container">
+          <img src="https://cdn.discordapp.com/attachments/889213230872928276/912458572116422676/ls_new_logo.png" alt="" class="herelogo">
+          <img src="https://cdn.discordapp.com/attachments/859173547997265921/896720034372669460/2.png" alt="" class="imgslide"> 
+        </div>
+      </div>
+
+      <!-- Slide 3 -->
+      <div class="carousel-item">
+        <div class="carousel-container">
+          <img src="https://cdn.discordapp.com/attachments/889213230872928276/912458572116422676/ls_new_logo.png" alt="" class="herelogo">
+          <img src="https://cdn.discordapp.com/attachments/859173547997265921/896720034372669460/2.png" alt="" class="imgslide"> 
+        </div>
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bx bx-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bx bx-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+
+
+
+</section>
+
+<section class="ekherhaja"  data-aos="fade-up" date-aos-delay="200">
+  <div class="Menu">
+    <ul class="Menu-list" data-offset="10">
+      <li class="Menu-list-item" data-offset="20" onclick>
+        Life STyle
+        <a href="#life"><span class="Mask"><span>Life STyle</span></span>
+          
+        <span class="Mask"><span>Life STyle</span></span></a> 
+      </li>
+      <a name="life"></a>
+      <li class="Menu-list-item" data-offset="12" onclick>
+        Our Team
+        <a href="#team"> <span class="Mask"><span>Our Team</span></span>
+        <span class="Mask"><span>Our Team</span></span></a>
+      </li>
+
+      <li class="Menu-list-item" data-offset="8" onclick>
+        Contact
+        <a href="#cont"><span class="Mask"><span>Contact</span></span>
+
+        <span class="Mask"><span>Contact</span></span></a>
+
+      </li>
+    </ul>
+  </div>
+
+</section>
+    <!-- begins Sec 3 -->
+    <section id="sec3 "  data-aos="fade-up" date-aos-delay="200">
+      
+      <center><div class="padd">
+        <span class="here">Life Style is a  tunisian FiveM serveur with strict rp , actif staff 24/7 and an exclusive development. Just Read the Rules , create your own story and join the LS Family</a>.</span>
+      </div></center>
+      
+
+
+    </section>
+    <!-- End SEC3 -->
+      <!-- ======= Why Us Section ======= -->
+      
+      <section class="why-us section-bg" data-aos="fade-up" date-aos-delay="200" >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 video-box">
+              <img src="https://cdn.discordapp.com/attachments/889213230872928276/912458572116422676/ls_new_logo.png" class="imgfluid" style="border-radius: 50%;" alt="">
+              <a href="https://www.youtube.com/watch?v=PYtYrtwG2Ew" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+            </div>
+  
+            <div class="col-lg-6 d-flex flex-column justify-content-center p-5">
+              
+              <div class="fonthere" >
+                <div class="site-content"> <script src="https://apis.google.com/js/platform.js" gapi_processed="true"></script> <div id="___ytsubscribe_0" style="text-indent: 0px; margin: 0px; padding: 0px; background: transparent; border-style: none; float: none; line-height: normal; font-size: 1px; vertical-align: baseline; display: inline-block; width: 182px; height: 48px;"><iframe ng-non-bindable="" frameborder="0" hspace="0" marginheight="0" marginwidth="0" scrolling="no" style="position: static; top: 0px; width: 182px; margin: 0px; border-style: none; left: 0px; visibility: visible; height: 48px; " tabindex="0" vspace="0" width="100%" id="I0_1634899756971" name="I0_1634899756971" src="https://www.youtube.com/subscribe_embed?usegapi=1&amp;channelid=UCzC4sgOOtYZozK_JZ4fabRg&amp;layout=full&amp;theme=default&amp;count=default&amp;origin=https%3A%2F%2Flakshmanbasnet.com&amp;gsrc=3p&amp;ic=1&amp;jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.fr.ty-JE5GGIi0.O%2Fam%3DAQ%2Fd%3D1%2Frs%3DAGLTcCNMla-z38-Wk6YsyUkimkx8PkDAnw%2Fm%3D__features__#_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart%2Concircled%2Cdrefresh%2Cerefresh%2Conload&amp;id=I0_1634899756971&amp;_gfid=I0_1634899756971&amp;parent=https%3A%2F%2Flakshmanbasnet.com&amp;pfname=&amp;rpctoken=50544674" data-gapiattached="true"></iframe></div></div>
+              </div>
+              
+                <p class="desc">This is our Youtube Channel and it will be actif with lives and videos ... Don't forget to subscribe and turn on the bill</p>
+                <a name="team"></a>
+            </div>
+            
+          </div>
+        </div>
+      </section><!-- End Why Us Section -->
+
+    <!-- begins Sec4 -->
+    
+    <section id="sec4"  data-aos="fade-up" date-aos-delay="200">
+      <center><div class="title"  data-aos="fade-up">Our Team</div></center><br><br>
+            <center><div class="left">This is our team who's actif 24/7 and always ready for any problem... Read More to discover the full team❤❤</div></center><br><br><br><br>
+                <div class="servicess">
+                    <center><img src="https://cdn.discordapp.com/attachments/903321156155760731/903332231194480640/LS-RP4.png" alt="Supervisors" id=""></center><br>
+                        <center><span class="yellow"> Supervisors</span></center><br>
+                        <span class="dowwn"> The supervisor are the leader of the staff team and he's who runs all the business.</span>
+                </div> 
+                <div class="servicess">
+                    <center><img src="https://cdn.discordapp.com/attachments/903321156155760731/903332231194480640/LS-RP4.png" alt="Owner" id=""></center><br>
+                        <center><span class="yellow"> Founders</span></center><br>
+                        <span class="dowwn"> The founders are the leaders of the serveur ...</span>
+                </div> 
+                
+                <div class="servicess service3">
+                    <center><img src="https://cdn.discordapp.com/attachments/903321156155760731/903332231194480640/LS-RP4.png" alt="Staff Team" id=""></center><br>
+                        <center><span class="yellow"> Staff Team</span></center><br>
+                        <span class="dowwn">This the Staff team who's the face of the serveur and the're the ones who provide all the conditions available for us to play freely .</span>   
+                </div>
+                 <br><br>
+                <center><a href="team.html" id="buuuut"> <button> READ MORE</button></a></center>
+                
+    </section>
+      <a name="cont"></a>
+    <form action="contact.php" method="post">
+      
+      <section class="ftco-section">
+        <div class="container">
+          <CENTER><div class="title con" id="responsive">CONTACT US</div></CENTER><br><br>
+          <div class="row justify-content-center">
+            <div class="col-lg-10 col-md-12">
+              <div class="wrapper">
+                <div class="row justify-content-center">
+                  <div class="col-lg-8">
+                    <div class="contact-wrap">
+                      <div id="form-message-warning" class="mb-4 w-100 text-center"></div> 
+                      <div id="form-message-success" class="mb-4 w-100 text-center">
+                        Your message was sent, thank you!
+                      </div>
+                      <form method="POST" id="contactForm" name="contactForm" class="contactForm">
+                        <div class="row">
+                          <div class="col-md-12">
+                            <div class="form-group">
+                              <input type="text" class="form-control" name="name" id="name" placeholder="Discord Name">
+                            </div>
+                          </div>
+                          <div class="col-md-12"> 
+                            <div class="form-group">
+                              <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                            </div>
+                          </div>
+                          <div class="col-md-12">
+                            <div class="form-group">
+                              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject">
+                            </div>
+                          </div>
+                          <div class="col-md-12">
+                            <div class="form-group">
+                              <textarea name="message" class="form-control" id="message" cols="30" rows="8" placeholder="Message"></textarea>
+                            </div>
+                          </div>
+                          <div class="col-md-12">
+                            <div class="form-group">
+                              <input type="submit" id="submit" name="submit" value="Send Message" class="btn btn-primary">
+                              <div class="submitting"></div>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </form>
+    <link rel="stylesheet" href="style.css">
+    
+                        
+                </div>
+
+  </main><!-- End #main -->
+
+
+
+
+  <!-- ======= Footer ======= -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="footer-col">
+          <h4>LIFE STYLE</h4>
+          <ul>
+            <li><a href="#contact">About us</a></li>
+          <li><a href="team.html">Our Team</a></li>
+          <li><a href="rules.html">Rules</a></li>
+          <li><a href="launcher.html">Launcher</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>TEST</h4>
+          <ul>
+            <li><a href="#">Test</a></li>
+            <li><a href="#">Test</a></li>
+            <li><a href="#">Test</a></li>
+            <li><a href="#">Test</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>TEST</h4>
+          <ul>
+            <li><a href="#" class="facebookfooter">Facebook</a></li>
+            <li><a href="#" class="instagramfooter">Instagram</a></li>
+            <li><a href="#" class="twitterfooter">Twitter</a></li>
+            <li><a href="#" class="youtubefooter">Youtube</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>follow us</h4>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+        </div>
+      </div><br><br>
+    </div>
+ </footer>
+  <!-- End Footer -->
+
+
+
+
+
+  <!-- php ends here -->
+
+  <a href="#hero" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  <script>
+    var loader = document.getElementById("preloader");
+    window.addEventListener("load" , function(){
+      loader.style.display = "none"
+    })
+  </script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+    AOS.init({
+      offset: 50,
+      duration: 50
+    });
+  </script>
+
+
+
+
+<script src="assets-pre/vendor/jquery/jquery.min.js"></script>
+<script src="assets-pre/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets-pre/vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="assets-pre/vendor/php-email-form/validate.js"></script>
+<script src="assets-pre/vendor/waypoints/jquery.waypoints.min.js"></script>
+<script src="assets-pre/vendor/counterup/counterup.min.js"></script>
+<script src="assets-pre/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets-pre/vendor/venobox/venobox.min.js"></script>
+<script src="assets-pre/vendor/owl.carousel/owl.carousel.min.js"></script>
+<script src="assets-pre/vendor/typed.js/typed.min.js"></script>
+<script src="assets-pre/vendor/aos/aos.js"></script>
+
+
+<!-- Template Main JS File -->
+<script src="assets-pre/js/main.js"></script>
+<script src="assets-pre/js/main.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="js/main.js"></script>
+
+
+
+
+
+</body>
+
+</html>
